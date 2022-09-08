06d1ff6fa5c80d7e5d10b3cca225d4377e8431d2
ibmcf <ibmcf1@aluno.ifal.edu.br>
2022-09-08 15:59:35 +0000

    Teste de integração de soma

---
+++ api-calculadora/src/test/java/app/IntegrationSoma.java
@@ -0,0 +1,30 @@
+package app;
+
+import io.jooby.JoobyTest;
+import io.jooby.StatusCode;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@JoobyTest(App.class)
+public class IntegrationSoma {
+
+  static OkHttpClient client = new OkHttpClient();
+
+  @Test
+  public void somar(int serverPort) throws IOException {
+    Request req = new Request.Builder()
+        .url("http://localhost:" + serverPort + "/soma/3/4")
+        .build();
+
+    try (Response rsp = client.newCall(req).execute()) {
+      assertEquals("7.0", rsp.body().string());
+      assertEquals(StatusCode.OK.value(), rsp.code());
+    }
+  }
+}
